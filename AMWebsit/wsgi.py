2eaa37bbb25dcab53c2563802da7602529a09ed5
csyu12 <215820356@qq.com>
2020-07-21 03:42:52 +0800

    更新项目配置

--- AMWebsit/wsgi.py
+++ AMWebsit/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AMSystem project.
+WSGI config for AMWebsit project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AMSystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AMWebsit.settings')
 
 application = get_wsgi_application()
